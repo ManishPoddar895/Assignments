b4b2379bd9f78ef572549732b8687b65a2f2386c
ManishPoddar895 <mpoddar@tavisca.com>
2018-03-21 16:53:45 +0530

    Software Installation Assignment

--- PowerShellAssignment/powershell.ps1
+++ PowerShellAssignment/powershell.ps1
@@ -4,6 +4,4 @@ $type=Read-Host -Prompt "Enter the type of log:"
 
 $t1=(Get-Date).AddHours(-$time)
 
-
-
 Get-EventLog -LogName "System" -After $t1 -EntryType $type | Export-Csv  -Path "C:\Users\mpoddar\Desktop\python\process.csv" -Delimiter ","
