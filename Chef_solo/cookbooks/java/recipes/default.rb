bf2fe6d32e7627be59ba82e3eeb3f47a42e823b1
ManishPoddar895 <mpoddar@tavisca.com>
2018-04-09 14:06:26 +0530

    Modified

--- Chef_solo/cookbooks/java/recipes/default.rb
+++ Chef_solo/cookbooks/java/recipes/default.rb
@@ -1,8 +1,12 @@
-
-java_ark "jdk" do
-    url node['java']['file']['url']
-    app_home node['java']['file']['path']
-    bin_cmds ["java", "javac"]
-    action :install
+remote_file '/var/ec2-user/' do
+  source 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-solaris-x64.tar.gz'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
 end
 
+
+execute 'tar' do
+  command 'tar -xvf /home/ec2-user/jdk-8u161-solaris-x64.tar'
+end
