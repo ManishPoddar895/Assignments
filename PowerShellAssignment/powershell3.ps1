b9fb1253be5cbccccc86523aae797ecdc3b87698
ManishPoddar895 <mpoddar@tavisca.com>
2018-04-09 15:32:36 +0530

    cleaned dir structure

--- PowerShellAssignment/powershell3.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-$url = "https://notepad-plus-plus.org/repository/7.x/7.5.6/npp.7.5.6.Installer.x64.exe"
-
-$outpath = "C:\Users\mpoddar\Downloads/myexe.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $outpath
-
-Start-Process -FilePath $outpath -ArgumentList "/S" -PassThru
-
