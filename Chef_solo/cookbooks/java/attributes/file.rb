4bc4ab802ee517f26c5ea2ed68a21cf3ada96add
ManishPoddar895 <mpoddar@tavisca.com>
2018-04-09 15:26:46 +0530

    added chef solo

---
+++ Chef_solo/cookbooks/java/attributes/file.rb
@@ -0,0 +1,8 @@
+default['java']['file']['name']= "java"
+default['java']['file']['version'] = '1.8.1'
+
+default['java']['file']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-solaris-x64.tar.gz'
+
+default['java']['file']['owner'] = 'root'
+default['java']['file']['group'] = 'root'
+default['java']['file']['mode']  = '0755'
---
+++ Chef_solo/cookbooks/java/recipes/default.rb
@@ -0,0 +1,30 @@
+
+# Cookbook:: java
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved
+
+
+
+=begin
+package ['java']['file']['name'] do
+  version ['java']['file']['version']
+  action :install
+end
+=end
+
+
+#Downloading java 
+remote_file '/home/ec2-user/download' do
+  source node['java']['file']['url']
+  owner default['java']['file']['owner']
+  group default['java']['file']['group']
+  mode default['java']['file']['mode']
+  action :create
+end
+
+
+#untar and Installing java
+execute 'tar' do
+  command 'tar -xvf /home/ec2-user/jdk-8u161-solaris-x64.tar'
+end
