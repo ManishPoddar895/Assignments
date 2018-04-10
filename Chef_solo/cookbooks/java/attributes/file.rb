70130ea6568a860f3ca5c8561b82743e19936e94
ManishPoddar895 <mpoddar@tavisca.com>
2018-04-10 09:05:11 +0530

    Added chefspec

--- Chef_solo/cookbooks/java/attributes/file.rb
+++ Chef_solo/cookbooks/java/attributes/file.rb
@@ -1,10 +1,10 @@
-default['java']['file']['name']= "java"
-default['java']['file']['version'] = '1.8.1'
+=begin
+default['java']['file']['url']= 'http://download.oracle.com/otn/java/jdk/6u29-b11/jdk-6u29-linux-x64.bin'
+default['java']['file']['path'] = '/usr/local/java/default'
+=end
 
-default['java']['file']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-solaris-x64.tar.gz'
 
-default['java']['file']['owner'] = 'root'
-default['java']['file']['group'] = 'root'
-default['java']['file']['mode']  = '0755'
+#defining attributes for java
+default['java']['file']['name'] = "install java"
+default['java']['file']['command'] = "yum install java-1.8.0-openjdk"
 
-default['java']['file']['tar'] = 'tar -xvf /home/ec2-user/jdk-8u161-solaris-x64.tar.qz'
--- Chef_solo/cookbooks/java/recipes/default.rb
+++ Chef_solo/cookbooks/java/recipes/default.rb
@@ -1,31 +1,41 @@
 
+#
 # Cookbook:: java
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved
 
 
-
 =begin
-package node['java']['file']['name'] do
-  version node['java']['file']['version']
-  action :install
-end
-=end
-
-
-#Downloading java 
-remote_file '/home/ec2-user/download' do
-  source node['java']['file']['url']
-  owner node['java']['file']['owner']
-  group node['java']['file']['group']
-  mode node['java']['file']['mode']
+remote_filr '/home/ec2-user/jdk-8u161-solaris-x64.tar.gz' do
+  source 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-solaris-x64.tar.gz'
+  owner 'root'
+  group 'root'
+  mode '0755'
   action :create
 end
 
-
-#untar and Installing java
-execute 'tar' do
-  command node['java']['file']['tar']
+bash 'unzip jdk package' do
+  code <<-EOH
+  tar -xvz /home/ec2-user/jdk-8u161-solaris-x64.tar.gz
+  EOH
 end
+
+=end
+
+
+#installing java 
+execute node['java']['file']['name'] do
+command node['java']['file']['command']
+end
+
+#installing tomcat
+tomcat_install 'helloworld' do
+  version '8.0.36'
+end
+
+
+
+
+
 
