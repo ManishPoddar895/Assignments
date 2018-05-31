f068352ffe3e0960d905adafbdfc1b445a7e3b76
ManishPoddar895 <mpoddar@tavisca.com>
2018-05-31 17:45:31 +0530

    Modified

--- shell.sh
+++ shell.sh
@@ -5,9 +5,4 @@ curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScrip
 unzip CloudWatchMonitoringScripts-1.2.2.zip
 rm CloudWatchMonitoringScripts-1.2.2.zip
 cd aws-scripts-mon
-cp awscreds.template awscreds.conf
-echo "AWSAccessKeyId=AKIAJMOJUMHVBQRL633A" > awscreds.conf
-echo "AWSSecretKey=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" >> awscreds.conf
-
-
 crontab -l | { cat; echo "* * * * * /opt/aws-scripts-mon/mon-put-instance-data.pl --mem-util  --auto-scaling=only >> /home/ec2-user/send.log"; } | crontab -
