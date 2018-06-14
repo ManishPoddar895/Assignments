6702f9d9145b90c8b7928c574d89479bc88bbb97
ManishPoddar895 <mpoddar@tavisca.com>
2018-06-14 18:18:23 +0530

    Chef POC

--- test1/cookbooks/demo/recipes/default.rb
+++ test1/cookbooks/demo/recipes/default.rb
@@ -3,7 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-file "C:/Users/mpoddar/Desktop/test1/hello.txt" do
-  content "Hello, this is my first cookbook recipe"
-  action :create
+#file "C:/Users/mpoddar/Desktop/test1/hello.txt" do
+#  content "Hello, this is my first cookbook recipe"
+#  action :create
+#end
+
+
+
+
+
+bash 'jenkins' do
+  code <<-EOH
+    wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+	sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+	sudo apt-get update
+	sudo apt-get install jenkins
+    EOH
 end
