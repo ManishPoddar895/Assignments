479bc61c09457dbeeec43f18f3c55fe5d5a24309
ManishPoddar895 <mpoddar@tavisca.com>
2018-06-18 11:14:25 +0530

    Java Script Added

--- javaInstall.sh
+++ javaInstall.sh
@@ -7,3 +7,5 @@ apt-cache search jdk
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk/bin
 javac -version
+curl -L https://www.opscode.com/chef/install.sh | bash
+chef-solo -v
